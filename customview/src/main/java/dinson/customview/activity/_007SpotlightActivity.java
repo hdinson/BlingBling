e7e639acb911a600354ec024b667b3a938653929
Dinson <991879363@qq.com>
2017-08-19 23:22:04 +0800

    新增高亮提示~

--- customview/src/main/java/dinson/customview/activity/_007SpotlightActivity.java
+++ customview/src/main/java/dinson/customview/activity/_007SpotlightActivity.java
@@ -1,11 +1,14 @@
 package dinson.customview.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import dinson.customview.R;
+import dinson.customview._global.BaseActivity;
+import dinson.customview.utils.TypefaceUtils;
 
-public class _007SpotlightActivity extends AppCompatActivity {
+public class _007SpotlightActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,23 @@ public class _007SpotlightActivity extends AppCompatActivity {
         setContentView(R.layout.activity__007_spotlight);
 
 
+        initUI();
+
+    }
+
+    private void initUI() {
+
+        Typeface typeface = TypefaceUtils.get(this, "fonts/FZLanTingHeiS_Regular.ttf");
+        TextView tv_title = (TextView) findViewById(R.id.tv_title);
+        TextView tv_dis = (TextView) findViewById(R.id.tv_dis);
+        TextView tv_type = (TextView) findViewById(R.id.tv_type);
+        TextView tv_name = (TextView) findViewById(R.id.tv_name);
+        TextView tv_content = (TextView) findViewById(R.id.tv_content);
+        tv_title.setTypeface(typeface);
+        tv_dis.setTypeface(typeface);
+        tv_type.setTypeface(typeface);
+        tv_name.setTypeface(typeface);
+        tv_content.setTypeface(typeface);
 
     }
 }
