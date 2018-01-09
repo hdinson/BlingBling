e71ef246b607e4d81fddab55f0dc707072db2712
dinson <dinson.me@gmail.com>
2018-01-09 21:57:35 +0800

    修正

--- customview/src/main/java/dinson/customview/entity/ClassBean.kt
+++ customview/src/main/java/dinson/customview/entity/ClassBean.kt
@@ -1,3 +1,3 @@
 package dinson.customview.entity
 
-class ClassBean(var title: String?, var desc: String?, var name: Class<*>?, var imgUrl: String?)
+data class ClassBean(var title: String?, var desc: String?, var name: Class<*>?, var imgUrl: String?)
