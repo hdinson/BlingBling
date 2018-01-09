78546cd7c5dae3bd7b7f962c0953355022dca882
Dinson <991879363@qq.com>
2018-01-09 21:57:35 +0800

    修正

--- customview/src/main/java/dinson/customview/activity/MainActivity.kt
+++ customview/src/main/java/dinson/customview/activity/MainActivity.kt
@@ -44,12 +44,13 @@ import kotlinx.android.synthetic.main.aspect_ratio_iv_layout.*
 
 class MainActivity : BaseActivity(), OnItemTouchMoveListener, OnItemClickListener.OnClickListener {
 
-    private val mContentData = ArrayList<ClassBean>() //内容的数据集
+    //private val mContentData = ArrayList<ClassBean>()
     private val mHeadData = ArrayList<DailyDetail>()
     private lateinit var mMainHeadAdapter: MainHeadAdapter
     private var mAMapLocationClient: AMapLocationClient? = null
     private lateinit var mTouchHelper: ItemTouchHelper
     private val mOneApi = HttpHelper.create(OneApi::class.java)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,42 +59,51 @@ class MainActivity : BaseActivity(), OnItemTouchMoveListener, OnItemClickListene
         initContent()
         initHead()
         getLocation()
+
+    }
+
+    /**
+     * 内容的数据集
+     */
+    private val mContentData: List<ClassBean> by lazy {
+        listOf(ClassBean(getString(R.string.facebook_title), getString(R.string.facebook_desc),
+            _001ShimmerActivity::class.java, getString(R.string.facebook_img)),
+            ClassBean(getString(R.string.qqnavi_title), getString(R.string.qqnavi_desc),
+                _002QQNaviViewActivity::class.java, getString(R.string.qqnavi_img)),
+            ClassBean(getString(R.string.exchange_rate_title), getString(R.string.exchange_rate_desc),
+                _003ExchangeActivity::class.java, getString(R.string.exchange_rate_img)),
+            ClassBean(getString(R.string.ganged_recycle_title), getString(R.string.ganged_recycle_desc),
+                _004GangedRecycleViewActivity::class.java, getString(R.string.ganged_recycle_img)),
+            ClassBean(getString(R.string.like_smile_title), getString(R.string.like_smile_desc),
+                _005LikeSmileViewActivity::class.java, getString(R.string.like_smile_img)),
+            ClassBean(getString(R.string.floatingView_title), getString(R.string.floatingView_desc),
+                _006FloatingViewActivity::class.java, getString(R.string.floatingView_img)),
+            ClassBean(getString(R.string.spotlight_title), getString(R.string.spotlight_desc),
+                _007SpotlightActivity::class.java, getString(R.string.spotlight_img)),
+            ClassBean(getString(R.string.rich_editor_title), getString(R.string.rich_editor_desc),
+                _008RichEditorActivity::class.java, getString(R.string.rich_editor_img)),
+            ClassBean(getString(R.string.google_vr_title), getString(R.string.google_vr_desc),
+                _009GoogleVRActivity::class.java, getString(R.string.google_vr_img)),
+            ClassBean(getString(R.string.parallax_animation_title), getString(R.string.parallax_animation_desc),
+                _010ParallaxActivity::class.java, getString(R.string.parallax_animation_img)),
+            ClassBean(getString(R.string.diagonal_layout_title), getString(R.string.diagonal_layout_desc),
+                _011DiagonalLayoutActivity::class.java, getString(R.string.diagonal_layout_img)),
+            ClassBean(getString(R.string.test_layout_title), getString(R.string.test_layout_desc),
+                TestActivity::class.java, getString(R.string.test_layout_img))
+        )
     }
 
     private fun initContent() {
-        mContentData.add(ClassBean(getString(R.string.facebook_title), getString(R.string.facebook_desc),
-            _001ShimmerActivity::class.java, getString(R.string.facebook_img)))
-        mContentData.add(ClassBean(getString(R.string.qqnavi_title), getString(R.string.qqnavi_desc),
-            _002QQNaviViewActivity::class.java, getString(R.string.qqnavi_img)))
-        mContentData.add(ClassBean(getString(R.string.exchange_rate_title), getString(R.string.exchange_rate_desc),
-            _003ExchangeActivity::class.java, getString(R.string.exchange_rate_img)))
-        mContentData.add(ClassBean(getString(R.string.ganged_recycle_title), getString(R.string.ganged_recycle_desc),
-            _004GangedRecycleViewActivity::class.java, getString(R.string.ganged_recycle_img)))
-        mContentData.add(ClassBean(getString(R.string.like_smile_title), getString(R.string.like_smile_desc),
-            _005LikeSmileViewActivity::class.java, getString(R.string.like_smile_img)))
-        mContentData.add(ClassBean(getString(R.string.floatingView_title), getString(R.string.floatingView_desc),
-            _006FloatingViewActivity::class.java, getString(R.string.floatingView_img)))
-        mContentData.add(ClassBean(getString(R.string.spotlight_title), getString(R.string.spotlight_desc),
-            _007SpotlightActivity::class.java, getString(R.string.spotlight_img)))
-        mContentData.add(ClassBean(getString(R.string.rich_editor_title), getString(R.string.rich_editor_desc),
-            _008RichEditorActivity::class.java, getString(R.string.rich_editor_img)))
-        mContentData.add(ClassBean(getString(R.string.google_vr_title), getString(R.string.google_vr_desc),
-            _009GoogleVRActivity::class.java, getString(R.string.google_vr_img)))
-        mContentData.add(ClassBean(getString(R.string.parallax_animation_title), getString(R.string.parallax_animation_desc),
-            _010ParallaxActivity::class.java, getString(R.string.parallax_animation_img)))
-        mContentData.add(ClassBean(getString(R.string.diagonal_layout_title), getString(R.string.diagonal_layout_desc),
-            _011DiagonalLayoutActivity::class.java, getString(R.string.diagonal_layout_img)))
-        mContentData.add(ClassBean(getString(R.string.test_layout_title), getString(R.string.test_layout_desc),
-            TestActivity::class.java, getString(R.string.test_layout_img)))
-
-
         val mainAdapter = MainContentAdapter(this, mContentData, this)
         mTouchHelper = ItemTouchHelper(MainItemTouchHelper(mainAdapter))
         mTouchHelper.attachToRecyclerView(rvContent)
-        rvContent.adapter = mainAdapter
-        rvContent.layoutManager = LinearLayoutManager(this)
-        rvContent.addItemDecoration(LinearItemDecoration(this))
-        rvContent.addOnItemTouchListener(OnItemClickListener(this, rvContent, this))
+
+        rvContent.apply {
+            adapter = mainAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            addItemDecoration(LinearItemDecoration(this@MainActivity))
+            addOnItemTouchListener(OnItemClickListener(this@MainActivity, rvContent, this@MainActivity))
+        }
     }
 
     private fun initHead() {
@@ -144,10 +154,11 @@ class MainActivity : BaseActivity(), OnItemTouchMoveListener, OnItemClickListene
 
     private fun getLocation() {
         mAMapLocationClient = AMapLocationClient(this.applicationContext)
-        val locationOption = getAmapOption()
-        mAMapLocationClient!!.setLocationOption(locationOption)//设置定位参数
-        mAMapLocationClient!!.setLocationListener(locationListener)//设置定位监听
-        mAMapLocationClient!!.startLocation()
+        mAMapLocationClient?.let {
+            it.setLocationListener(locationListener)//设置定位监听
+            it.setLocationOption(getAmapOption())//设置定位参数
+            it.startLocation()//开始定位
+        }
     }
 
 
@@ -180,9 +191,11 @@ class MainActivity : BaseActivity(), OnItemTouchMoveListener, OnItemClickListene
         LogUtils.d("." + weather.toString())
         weatherLayout.visibility = View.VISIBLE
         val resultsBean = weather.results[0]
-        if_weather.setText(HomeWeatherModelUtil.getWeatherFont(resultsBean.now.code))
-        tvWeather.typeface = TypefaceUtils.get(this, "fonts/FZLanTingHeiS_Regular.ttf")
-        tvWeather.text = String.format("%s℃", resultsBean.now.temperature)
+        iconFontWeather.apply {
+            setText(HomeWeatherModelUtil.getWeatherFont(resultsBean.now.code))
+            typeface = TypefaceUtils.get(this@MainActivity, "fonts/FZLanTingHeiS_Regular.ttf")
+            text = String.format("%s℃", resultsBean.now.temperature)
+        }
 
         val alphaAnimator = ObjectAnimator.ofFloat(weatherLayout, "alpha", 0f, 1f)
         val scaleX = ObjectAnimator.ofFloat(weatherLayout, "scaleX", 0f, 1.0f)
@@ -208,19 +221,22 @@ class MainActivity : BaseActivity(), OnItemTouchMoveListener, OnItemClickListene
      */
     private fun getAmapOption(): AMapLocationClientOption {
         val mOption = AMapLocationClientOption()
-        mOption.locationMode = AMapLocationClientOption.AMapLocationMode.Battery_Saving
-        //可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
-        mOption.isGpsFirst = false//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
-        mOption.httpTimeOut = 30000//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
-        mOption.interval = 2000//可选，设置定位间隔。默认为2秒
-        mOption.isNeedAddress = true//可选，设置是否返回逆地理地址信息。默认是true
-        mOption.isOnceLocation = true//可选，设置是否单次定位。默认是false
-        mOption.isOnceLocationLatest = false//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
+        mOption.apply {
+            //可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
+            locationMode = AMapLocationClientOption.AMapLocationMode.Battery_Saving
+            isGpsFirst = false//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
+            httpTimeOut = 30000//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
+            interval = 2000//可选，设置定位间隔。默认为2秒
+            isNeedAddress = true//可选，设置是否返回逆地理地址信息。默认是true
+            isOnceLocation = true//可选，设置是否单次定位。默认是false
+            isOnceLocationLatest = false//可选，是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
+            isSensorEnable = false//可选，设置是否使用传感器。默认是false
+            //可选，是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
+            isWifiScan = true
+            isLocationCacheEnable = true //可选，设置是否使用缓存定位，默认为true
+        }
         AMapLocationClientOption.setLocationProtocol(AMapLocationClientOption.AMapLocationProtocol.HTTP)//可选，
         // 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
-        mOption.isSensorEnable = false//可选，设置是否使用传感器。默认是false
-        mOption.isWifiScan = true //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
-        mOption.isLocationCacheEnable = true //可选，设置是否使用缓存定位，默认为true
         return mOption
     }
 
