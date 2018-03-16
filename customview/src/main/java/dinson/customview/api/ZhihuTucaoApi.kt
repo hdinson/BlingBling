25abcf53da6948ce14cb2523236b37b91f546eba
Dinson <991879363@qq.com>
2018-03-16 16:18:38 +0800

    知乎吐槽列表

--- customview/src/main/java/dinson/customview/api/ZhihuTucaoApi.kt
+++ customview/src/main/java/dinson/customview/api/ZhihuTucaoApi.kt
@@ -3,6 +3,7 @@ package dinson.customview.api
 import dinson.customview.entity.zhihu.ZhihuTucaoListResponse
 import io.reactivex.Observable
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 /**
  * 知乎相关api
@@ -13,6 +14,12 @@ interface ZhihuTucaoApi {
      */
     @GET("http://news-at.zhihu.com/api/4/section/2")
     fun getStoriesList(): Observable<ZhihuTucaoListResponse>
+
+    /**
+     * 获取知乎指定时间以前的吐槽列表
+     */
+    @GET("http://news-at.zhihu.com/api/4/section/2/before/{timestamp}")
+    fun getStoriesListBeforeData(@Path("timestamp") timestamp: Int): Observable<ZhihuTucaoListResponse>
 
 
 }
