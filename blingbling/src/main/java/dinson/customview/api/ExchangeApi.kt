7703f5e7e828e7a551a02c3fe98df898ca7cef3f
dinson <dinson.me@gmail.com>
2018-04-10 19:38:32 +0800

    。。

--- blingbling/src/main/java/dinson/customview/api/ExchangeApi.kt
+++ blingbling/src/main/java/dinson/customview/api/ExchangeApi.kt
@@ -9,9 +9,7 @@ import retrofit2.http.Headers
  *  汇率兑换接口
  */
 interface ExchangeApi{
-
     @Headers("Cache-Control:public,max-age=300")
     @GET("https://openexchangerates.org/api/latest.json?app_id=cf7a69aa7d174a4e8528a6a92b107903")
     fun getRate():Observable<ExchangeBean>
-
 }
