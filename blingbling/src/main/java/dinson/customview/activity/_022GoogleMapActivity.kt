dc650cc19fc21770fab8341b7eb2f8ece9657bf5
dinson <dinson.me@gmail.com>
2018-04-24 17:55:35 +0800

    fix

--- blingbling/src/main/java/dinson/customview/activity/_022GoogleMapActivity.kt
+++ blingbling/src/main/java/dinson/customview/activity/_022GoogleMapActivity.kt
@@ -2,6 +2,7 @@ package dinson.customview.activity
 
 import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.annotation.SuppressLint
+import android.content.Context
 import android.location.Location
 import android.os.Bundle
 import com.google.android.gms.common.ConnectionResult
@@ -18,6 +19,14 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.tbruyelle.rxpermissions2.RxPermissions
 import dinson.customview.R
 import dinson.customview._global.BaseActivity
+import dinson.customview.kotlin.info
+import com.google.android.gms.location.LocationAvailability
+import android.content.Context.LOCATION_SERVICE
+import android.location.LocationManager
+
+
+
+
 
 
 class _022GoogleMapActivity : BaseActivity(),
@@ -48,12 +57,11 @@ class _022GoogleMapActivity : BaseActivity(),
         mapFragment.getMapAsync(this)
 
 
-
     }
 
     override fun onStart() {
         super.onStart()
-        mGoogleApiClient.context
+        mGoogleApiClient.connect()
     }
 
     override fun onStop() {
@@ -66,14 +74,43 @@ class _022GoogleMapActivity : BaseActivity(),
      * This is where we can add markers or lines, add listeners or move the camera.
      * Device must installed Google Play services.
      */
-    @SuppressLint("MissingPermission")
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
 
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        info("onMapReady...")
+
+        mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.setOnMarkerClickListener(this)
         // Add a marker in Sydney and move the camera
         /*val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))*/
+        val myPlace = LatLng(40.73, -73.99)  // this is New York
+        mMap.addMarker(MarkerOptions().position(myPlace).title("My Favorite City"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPlace, 12f))
+    }
+
+    /**
+     * GoogleMap连接暂停
+     */
+    override fun onConnectionSuspended(p0: Int) {
+        info("onConnectionSuspended...")
+    }
+
+    /**
+     * GoogleMap连接失败
+     */
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        info("onConnectionFailed...")
+    }
+
+    /**
+     * GoogleMap连接完毕
+     */
+    @SuppressLint("MissingPermission")
+    override fun onConnected(p0: Bundle?) {
+        info("onConnected...")
 
 
         RxPermissions(this).request(ACCESS_COARSE_LOCATION)
@@ -92,7 +129,7 @@ class _022GoogleMapActivity : BaseActivity(),
                  info(myLocation.toString() + "-----")*/
 
                 mMap.isMyLocationEnabled = true
-                val locationAvailability =
+               /* val locationAvailability =
                     LocationServices.getFusedLocationProviderClient(this).locationAvailability
                 //LocationServices.FusedLocationApi.getLocationAvailability(mGoogleApiClient)
                 if (locationAvailability.isSuccessful) {
@@ -100,36 +137,60 @@ class _022GoogleMapActivity : BaseActivity(),
                     LocationServices.getFusedLocationProviderClient(this).lastLocation.let {
                         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(it.result.latitude, it.result.longitude), 12f))
                     }
+                }*/
+                // 2
+
+
+                info("------1")
+
+
+                val locationManager = getSystemService (Context.LOCATION_SERVICE) as LocationManager
+               /* locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000, 0f, )*/
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000, 0f,object :android.location.LocationListener{
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onProviderEnabled(provider: String?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onProviderDisabled(provider: String?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onLocationChanged(location: Location?) {
+
+
+                    }
+
+                })
+
+
+
+                val locationAvailability = LocationServices.FusedLocationApi.getLocationAvailability(mGoogleApiClient)
+                if (null != locationAvailability && locationAvailability.isLocationAvailable) {
+                    // 3
+                    info("------2")
+                    val  mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient)
+                    // 4
+                    if (mLastLocation != null) {
+                        info("------3")
+                        val currentLocation = LatLng(mLastLocation.latitude, mLastLocation
+                            .longitude)
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 12f))
+                    }
                 }
             }
-
-    }
-
-    /**
-     * GoogleMap连接暂停
-     */
-    override fun onConnectionSuspended(p0: Int) {
-
-    }
-
-    /**
-     * GoogleMap连接失败
-     */
-    override fun onConnectionFailed(p0: ConnectionResult) {
-
-    }
-
-    /**
-     * GoogleMap连接完毕
-     */
-    override fun onConnected(p0: Bundle?) {
-
     }
 
     /**
      * GoogleMarkerClick点击
      */
     override fun onMarkerClick(p0: Marker?): Boolean {
+        info("onMarkerClick...")
         return true
     }
 
@@ -137,7 +198,7 @@ class _022GoogleMapActivity : BaseActivity(),
      * 定位回调
      */
     override fun onLocationChanged(p0: Location?) {
-
+        info("onLocationChanged...")
 
     }
 }
