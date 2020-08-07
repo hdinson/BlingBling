9b1b204f3560edcbafada8f9c7ad6a8eed3a4cb9
dinson <dinson.me@gmail.com>
2020-08-07 10:27:08 +0800

    更改jcenter 上传脚本

--- blingbase/src/main/java/com/dinson/blingbase/widget/RatioImageView.kt
+++ blingbase/src/main/java/com/dinson/blingbase/widget/RatioImageView.kt
@@ -6,28 +6,55 @@ import androidx.appcompat.widget.AppCompatImageView
 import com.dinson.blingbase.R
 
 /**
- * 比例的ImageView
+ * 自带比例的ImageView
  */
-@Suppress("unused")
 class RatioImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : AppCompatImageView(context, attrs) {
 
     private var mRatio: Float
+    private var mOriginalWidth = -1
+    private var mOriginalHeight = -1
 
     init {
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.RatioImageView)
-        mRatio = attributes.getFloat(R.styleable.RatioImageView_imgRatio, 1f)
+        mRatio = attributes.getFloat(R.styleable.RatioImageView_rivImgRatio, 1f)
         attributes.recycle()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
-        setMeasuredDimension(widthSize, (widthSize * mRatio).toInt())
+        if (mOriginalWidth > 0 && mOriginalHeight > 0) {
+            val ratio = mOriginalWidth.toFloat() / mOriginalHeight
+            var width = MeasureSpec.getSize(widthMeasureSpec)
+            var height = MeasureSpec.getSize(heightMeasureSpec)
+            if (width > 0) {
+                height = (width.toFloat() / ratio).toInt()
+            } else if (height > 0) {
+                width = (height.toFloat() * ratio).toInt()
+            }
+            setMeasuredDimension(width, height)
+        } else {
+            val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+            setMeasuredDimension(widthSize, (widthSize * mRatio).toInt())
+        }
     }
 
+    /**
+     * 设置比例
+     * @param ratio 宽高比
+     */
     fun setRatio(ratio: Float) {
         mRatio = ratio
         requestLayout()
+    }
+
+    /**
+     * 设置原始尺寸比例
+     * @param originalHeight 原始尺寸高度
+     * @param originalWidth 原始尺寸宽度
+     */
+    fun setOriginalSize(originalWidth: Int, originalHeight: Int) {
+        mOriginalWidth = originalWidth
+        mOriginalHeight = originalHeight
     }
 }
